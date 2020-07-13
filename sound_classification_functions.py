6d75b9eda536ad2000602e5d64a63f4fe6046f34
LAPTOP-1RN5HSA5\Russel King <kevochi09@gmail.com>
2020-07-13 16:29:57 +0300

    first commit to kick it off.

---
+++ sound_classification_functions.py
@@ -0,0 +1,12 @@
+#sound classification with Tensorflow using ConvNets and Keras
+
+#libraries
+import pathlib
+from pathlib import Path 
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
